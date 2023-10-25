f7913411119a1033b046b1fbdf3e8061d508896e
Lokesh Varathan <97378529+lowkeyOwO@users.noreply.github.com>
2023-10-25 22:11:58 +0530

    Update FirebaseConfig.ts

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -4,12 +4,7 @@ import {getFirestore} from 'firebase/firestore'
  
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBA1LpIZ8HJPuy9PT5WfNunCr9vnAzCEw",
-  authDomain: "pecunia-aadc8.firebaseapp.com",
-  projectId: "pecunia-aadc8",
-  storageBucket: "pecunia-aadc8.appspot.com",
-  messagingSenderId: "1091579973072",
-  appId: "1:1091579973072:web:d883c4d0be7c06e548fdd5"
+  //Enter Config details
 };
 
 // Initialize Firebase
